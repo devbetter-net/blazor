327d6fd572f59040635eddbfa08dbf79e9facb12
Khoa Nguyen <khoa@chitmeo.com>
2024-08-31 23:02:07 +0700

    update

---
+++ Dev/BlazorApp/Data/PizzaStoreContext.cs
@@ -0,0 +1,13 @@
+﻿using BlazorApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp.Datal;
+
+public class PizzaStoreContext : DbContext
+{
+    public PizzaStoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<PizzaSpecial> Specials { get; set; }
+}
--- Dev/BlazorApp/Program.cs
+++ Dev/BlazorApp/Program.cs
@@ -1,4 +1,6 @@
 using BlazorApp.Components;
+using BlazorApp.Data;
+using BlazorApp.Datal;
 
 namespace BlazorApp
 {
@@ -11,6 +13,12 @@ namespace BlazorApp
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
+            builder.Services.AddControllers();
+            
+            builder.Services.AddSingleton<PizzaService>();
+            // Register the pizzas service
+            builder.Services.AddHttpClient();
+            builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 
             var app = builder.Build();
 
@@ -29,6 +37,22 @@ namespace BlazorApp
 
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
+
+            // Initialize the database
+            var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+                if (db.Database.EnsureCreated())
+                {
+                    SeedData.Initialize(db);
+                }
+            }
+
+            //app.MapRazorPages();
+            //app.MapBlazorHub();
+            //app.MapFallbackToPage("/_Host");
+            app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
