bc7a924bdbae3c4f5c7fda2518707a3fd192a512
kyle <51529656+devbetter-net@users.noreply.github.com>
2024-09-01 06:18:16 +0700

    3 interact with data in blazor web apps (#6)
    * update
    
    * add create_user_interface
    
    ---------
    
    Co-authored-by: Khoa Nguyen <khoa@chitmeo.com>

---
+++ Dev/BlazorApp/Data/PizzaService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorApp.Models;
+
+namespace BlazorApp.Data;
+
+public class PizzaService
+{
+    public Task<Pizza[]> GetPizzasAsync()
+    {
+        return Task.FromResult(new Pizza[0]);
+    }
+}
